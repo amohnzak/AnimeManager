921dd16a83f09676f989bbedad27a1bf6d14b49c
Aqlan Najwan Mohd Zakil Hussin <aqlan.najwan@gmail.com>
2019-08-10 20:13:10 -0400

    Initial Commit

---
+++ AnimeManager/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AnimeManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dataPath = "D:\\Users\\Aqlan Najwan\\Documents\\Anime Manager\\";
+
+            DirectoryInfo mainFolder = new DirectoryInfo(dataPath);
+            foreach (DirectoryInfo animeFolder in mainFolder.EnumerateDirectories())
+            {
+                string animeTitle = animeFolder.Name;
+
+
+            }
+        }
+    }
+}
